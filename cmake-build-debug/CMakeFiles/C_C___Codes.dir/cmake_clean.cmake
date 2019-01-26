d8911540dd9850d59d21a1071f0cf81698780909
SoumitSaha <1505047.sks@ugrad.cse.buet.ac.bd>
2019-01-26 18:35:12 +0600

    ABP done

---
+++ cmake-build-debug/CMakeFiles/C_C___Codes.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_C___Codes.dir/Simulator.cpp.obj"
+  "C_C___Codes.pdb"
+  "C_C___Codes.exe"
+  "C_C___Codes.exe.manifest"
+  "libC_C___Codes.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C_C___Codes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ structures.h
@@ -0,0 +1,28 @@
+/********* FUNCTION PROTOTYPES. DEFINED IN THE LATER PART******************/
+void starttimer(int AorB, float increment);
+void stoptimer(int AorB);
+void tolayer3(int AorB, struct pkt packet);
+void tolayer5(int AorB, char datasent[20]);
+
+#define BIDIRECTIONAL 0 /* change to 1 if you're doing extra credit */
+/* and write a routine called B_output */
+
+
+/* a "msg" is the data unit passed from layer 5 (teachers code) to layer  */
+/* 4 (students' code).  It contains the data (characters) to be delivered */
+/* to layer 5 via the students transport level protocol entities.         */
+struct msg
+{
+    char data[20];
+};
+
+/* a packet is the data unit passed from layer 4 (students code) to layer */
+/* 3 (teachers code).  Note the pre-defined packet structure, which all   */
+/* students must follow. */
+struct pkt
+{
+    int seqnum;
+    int acknum;
+    int checksum;
+    char payload[20];
+};
